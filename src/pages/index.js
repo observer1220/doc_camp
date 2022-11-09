ea89cf4023a2cc1188b1ce1f5d920f335b5b8219
observer1220 <observer1220@gmail.com>
2022-11-09 17:55:29 +0800

    update 2022-11-09 17:55:29

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
@@ -7,9 +7,16 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import styles from './index.module.css';
 import { Carousel } from 'antd';
 import 'antd/dist/antd.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { Parallax } from 'react-parallax';
 
 function HomepageHeader () {
   const { siteConfig } = useDocusaurusContext();
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -37,19 +44,23 @@ export default function Home () {
     <Layout
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
-      <Carousel afterChange={onChange} autoplay effect="fade" dotPosition="right">
-        <img src='./img/camp/a-4.jpg' />
-        <img src='./img/camp/b-4.jpg' />
-        <img src='./img/camp/c-3.jpg' />
-        <img src='./img/camp/d-2.jpg' />
-        <img src='./img/camp/e-5.jpg' />
-        <img src='./img/camp/f-5.jpg' />
-      </Carousel>
+        {/* <Carousel afterChange={onChange} autoplay effect="fade" dotPosition="right">
+          <img src='./img/camp/a-4.jpg' />
+          <img src='./img/camp/b-4.jpg' />
+          <img src='./img/camp/c-3.jpg' />
+          <img src='./img/camp/d-2.jpg' />
+          <img src='./img/camp/e-5.jpg' />
+          <img src='./img/camp/f-5.jpg' />
+          </Carousel> */}
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        {/* <Parallax bgImage="./img/camp/a-4.jpg" blur={0} strength={1000} style={{ height: '300px' }}>
+          Content goes here. Parallax height grows with content height.
+        </Parallax> */}
+        {/* <div data-aos="fade-up" > */}
+          <HomepageFeatures />
+        {/* </div> */}
       </main>
-
     </Layout>
   );
 }
