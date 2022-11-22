ef46ddfd3fd4cb3526b68e91468f6f6d52eba4e6
observer1220 <observer1220@gmail.com>
2022-11-23 00:19:48 +0800

    update 2022-11-23 00:19:48

--- src/pages/dynamic/index.js
+++ src/pages/dynamic/index.js
@@ -1,54 +1,14 @@
 import React, { useState } from "react";
-import Layout from "@theme/Layout";
 import ReactJkMusicPlayer from 'react-jinke-music-player'
 import 'react-jinke-music-player/assets/index.css'
 import { Button } from "antd";
-
-const audioList1 = [
-  {
-    name: 'Despacito',
-    singer: 'Luis Fonsi',
-    cover:
-      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
-    musicSrc:
-      'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
-  },
-  {
-    name: 'Dorost Nemisham',
-    singer: 'Sirvan Khosravi',
-    cover:
-      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
-    musicSrc:
-      'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
-  },
-]
-
-const audioList2 = [
-  {
-    name: 'Bedtime Stories',
-    singer: 'Jay Chou',
-    cover:
-      'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
-    musicSrc:
-      'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
-  }
-]
+import { audioList1, audioList2 } from './mock'
 
 export default function DemoPage () {
   const [data, setData] = useState()
-  
-  const updateParams = (params) => {
-    const newData = {
-      ...data, params
-    }
-    setData({
-      params: newData
-    })
-  }
 
-  const onChangeToFirstAudioList = () => {
-    console.log('1');
-    updateParams({
+  const onChangeToFirstAudioList = (index) => {
+    setData({
       clearPriorAudioLists: true,
       quietUpdate: false,
       audioLists: audioList1,
@@ -56,31 +16,27 @@ export default function DemoPage () {
   }
 
   const onChangeToSecondAudioList = () => {
-    console.log('2');
-    updateParams({  
+    setData({
       clearPriorAudioLists: true,
       quietUpdate: false,
       audioLists: audioList2,
     })
   }
+
   return <>
     <div className="settings">
       <Button type="button" onClick={onChangeToFirstAudioList}>
-        前張專輯 ({audioList1.length})
+        西洋最新 ({audioList1.length})
       </Button>
       <Button type="button" onClick={onChangeToSecondAudioList}>
-        下張專輯 ({audioList2.length})
+        周杰倫 八度空間 ({audioList2.length})
       </Button>
     </div>
     <ReactJkMusicPlayer
-    {...data}
-      // audioLists={audioList1}
+      {...data}
       mode={"full"}
       autoHiddenCover={true}
       showLyric={true}
-      onAudioPlayTrackChange={(event) => {
-        console.log(event);
-      }}
       volumeFade={{ fadeIn: 500, fadeOut: 500 }}
     />
   </>
---
+++ src/pages/dynamic/mock.js
@@ -0,0 +1,70 @@
+export const audioList1 = [
+  {
+    name: 'Despacito',
+    singer: 'Luis Fonsi',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
+    musicSrc:
+      'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
+  },
+  {
+    name: 'Dorost Nemisham',
+    singer: 'Sirvan Khosravi',
+    cover:
+      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
+  },
+  {
+    name: 'Bedtime Stories',
+    singer: 'Jay Chou',
+    cover:
+      'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
+    musicSrc:
+      'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
+  }
+]
+
+export const audioList2 = [
+
+  {
+    name: '半獸人',
+    singer: 'Jay Chou',
+    cover:
+      'https://res.cloudinary.com/dbjxyxxlj/image/upload/v1669133560/JayChou%EF%BC%8DOctaveSpace/Jay_2002_ablum_cover_ikubuk.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/dbjxyxxlj/video/upload/v1669133301/JayChou%EF%BC%8DOctaveSpace/%E5%8D%8A%E7%8D%B8%E4%BA%BA_ebxlrb.mp3',
+  },
+  {
+    name: '半島鐵盒',
+    singer: 'Jay Chou',
+    cover:
+      'https://res.cloudinary.com/dbjxyxxlj/image/upload/v1669133560/JayChou%EF%BC%8DOctaveSpace/Jay_2002_ablum_cover_ikubuk.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/dbjxyxxlj/video/upload/v1669133302/JayChou%EF%BC%8DOctaveSpace/%E5%8D%8A%E5%B3%B6%E9%90%B5%E7%9B%92_jyxwba.mp3',
+  },
+  {
+    name: '火車叨位去',
+    singer: 'Jay Chou',
+    cover:
+      'https://res.cloudinary.com/dbjxyxxlj/image/upload/v1669133560/JayChou%EF%BC%8DOctaveSpace/Jay_2002_ablum_cover_ikubuk.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/dbjxyxxlj/video/upload/v1669133300/JayChou%EF%BC%8DOctaveSpace/%E7%81%AB%E8%BB%8A%E5%8F%A8%E4%BD%8D%E5%8E%BB_zhagrd.mp3',
+  },
+  {
+    name: '爺爺泡的茶',
+    singer: 'Jay Chou',
+    cover:
+      'https://res.cloudinary.com/dbjxyxxlj/image/upload/v1669133560/JayChou%EF%BC%8DOctaveSpace/Jay_2002_ablum_cover_ikubuk.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/dbjxyxxlj/video/upload/v1669133300/JayChou%EF%BC%8DOctaveSpace/%E7%88%BA%E7%88%BA%E6%B3%A1%E7%9A%84%E8%8C%B6_ev1pny.mp3',
+  },
+  {
+    name: '最後的戰役',
+    singer: 'Jay Chou',
+    cover:
+      'https://res.cloudinary.com/dbjxyxxlj/image/upload/v1669133560/JayChou%EF%BC%8DOctaveSpace/Jay_2002_ablum_cover_ikubuk.jpg',
+    musicSrc:
+      'https://res.cloudinary.com/dbjxyxxlj/video/upload/v1669133302/JayChou%EF%BC%8DOctaveSpace/%E6%9C%80%E5%BE%8C%E7%9A%84%E6%88%B0%E5%BD%B9_ososej.mp3',
+  },
+]
