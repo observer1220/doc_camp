755e33f8e0f6ce1e875d97ff4cecb382f4d2d31d
observer1220 <observer1220@gmail.com>
2022-11-22 18:09:58 +0800

    update 2022-11-22 18:09:58

--- src/pages/dynamic/index.js
+++ src/pages/dynamic/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "@theme/Layout";
 import ReactJkMusicPlayer from 'react-jinke-music-player'
 import 'react-jinke-music-player/assets/index.css'
+import { Button } from "antd";
 
 const audioList1 = [
   {
@@ -30,36 +31,18 @@ const audioList2 = [
       'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
     musicSrc:
       'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
-  },
-  {
-    name: 'Dorost Nemisham',
-    singer: 'Sirvan Khosravi',
-    cover:
-      'https://res.cloudinary.com/ehsanahmadi/image/upload/v1573758778/Sirvan-Khosravi-Dorost-Nemisham_glicks.jpg',
-    musicSrc: () => {
-      return Promise.resolve(
-        'https://res.cloudinary.com/ehsanahmadi/video/upload/v1573550770/Sirvan-Khosravi-Dorost-Nemisham-128_kb8urq.mp3',
-      )
-    },
-  },
-  {
-    name: 'Despacito',
-    singer: 'Luis Fonsi',
-    cover:
-      'http://res.cloudinary.com/alick/image/upload/v1502689731/Despacito_uvolhp.jpg',
-    musicSrc:
-      'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
-  },
+  }
 ]
 
 export default function DemoPage () {
+  const [data, setData] = useState()
+  
   const updateParams = (params) => {
-    const data = {
-      ...this.state.params,
-      ...params,
+    const newData = {
+      ...data, params
     }
-    this.setState({
-      params: data,
+    setData({
+      params: newData
     })
   }
 
@@ -74,23 +57,24 @@ export default function DemoPage () {
 
   const onChangeToSecondAudioList = () => {
     console.log('2');
-    updateParams({
+    updateParams({  
       clearPriorAudioLists: true,
       quietUpdate: false,
       audioLists: audioList2,
     })
   }
   return <>
-    <section className="settings">
-      <button type="button" onClick={onChangeToFirstAudioList}>
-        change to first audio list ({audioList1.length})
-      </button>
-      <button type="button" onClick={onChangeToSecondAudioList}>
-        change to second audio list ({audioList2.length})
-      </button>
-    </section>
+    <div className="settings">
+      <Button type="button" onClick={onChangeToFirstAudioList}>
+        前張專輯 ({audioList1.length})
+      </Button>
+      <Button type="button" onClick={onChangeToSecondAudioList}>
+        下張專輯 ({audioList2.length})
+      </Button>
+    </div>
     <ReactJkMusicPlayer
-      audioLists={audioList1}
+    {...data}
+      // audioLists={audioList1}
       mode={"full"}
       autoHiddenCover={true}
       showLyric={true}
